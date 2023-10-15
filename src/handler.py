c042f827ef2a6988769f2c4ee308dbb758ecd502
nstankov-bg <me@nstankov.com>
2023-10-15 10:45:21 -0400

    automated

--- src/handler.py
+++ src/handler.py
@@ -1,37 +1,60 @@
-# Imports
 import json
 import logging
 import os
 import time
 from typing import Any, Dict, Union
 
-# Constants and Configurations
 HTTP_OK = 200
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
-# Initialize Logging
-logging.basicConfig(level=LOG_LEVEL)
+WARM_START_INDICATOR = None
+
 logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(LOG_LEVEL)
 
 
-def fetch_result() -> str:
-    """Simulated function that fetches the result."""
-    return "Fast execution"
+def fetch_result(is_warm_start: bool) -> str:
+    if is_warm_start:
+        return "Fast execution due to warm start"
+    else:
+        time.sleep(2)
+        return "Slow execution due to cold start"
 
 
 def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Union[int, str]]:
-    """AWS Lambda function handler."""
-    start_time = time.time()
-    logger.info(f"Received event: {json.dumps(event)}")
+    global WARM_START_INDICATOR
 
-    # Fetch the result
-    result = fetch_result()
+    start_time = time.perf_counter()
 
-    end_time = time.time()
+    if logger.isEnabledFor(logging.INFO):
+        logger.info(f"Received event: {json.dumps(event)}")
+
+    is_warm_start = WARM_START_INDICATOR is not None
+    WARM_START_INDICATOR = True
+
+    result = fetch_result(is_warm_start)
+
+    end_time = time.perf_counter()
     execution_time = end_time - start_time
-    logger.info(f"Execution time: {execution_time} seconds")
 
-    return {
+    response = {
         "statusCode": HTTP_OK,
-        "body": json.dumps({"result": result, "execution_time": execution_time}),
+        "body": json.dumps(
+            {
+                "result": result,
+                "execution_time": execution_time,
+                "is_warm_start": is_warm_start,
+                "memory_limit": context.memory_limit_in_mb,
+                "remaining_time": context.get_remaining_time_in_millis(),
+            }
+        ),
     }
+
+    if logger.isEnabledFor(logging.INFO):
+        logger.info(f"Execution Summary: {json.dumps(response)}")
+
+    return response
